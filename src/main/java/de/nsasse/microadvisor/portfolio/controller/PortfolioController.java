537657c7ad86e64fa25507ab62a9c419a66b1659
nsasse <nico.sasse@cofinpro.de>
2020-03-25 17:47:54 +0100

    update post requests

--- src/main/java/de/nsasse/microadvisor/portfolio/controller/PortfolioController.java
+++ src/main/java/de/nsasse/microadvisor/portfolio/controller/PortfolioController.java
@@ -1,24 +1,20 @@
 package de.nsasse.microadvisor.portfolio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.ManagedBean;
 
 @ManagedBean
 @RestController
-@RequestMapping("/portfolio/")
+@RequestMapping(value = "/portfolio/", method = RequestMethod.POST)
 public class PortfolioController {
 
-    //TODO - NOT WORKING
-    @GetMapping("/risk")
-    public void getRisk(@RequestParam(name = "id") String string) {
-        //System.out.println(riskProfile.getExpectedYield());
-        //System.out.println(riskProfile.getDuration());
-        //System.out.println(riskProfile.getRiskTolerance());
-        System.out.println(string);
+    @CrossOrigin
+    @GetMapping("/riskprofile")
+    public String getRisk(String test) {
+        System.out.println("GET");
+        System.out.println(test);
+        return "posting";
     }
 }
 
